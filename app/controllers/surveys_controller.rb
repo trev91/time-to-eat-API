039f5ec859063658673a936d5aad4ee4616b1643
Trevor Hardy <trevor@saharadev.com>
2017-06-02 14:17:37 -0600

    id suvery fix

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -12,7 +12,7 @@ class SurveysController < ApplicationController
   def show
     @survey = Survey.find(params[:id])
 
-    render json: @survey, include: ‘questions.responses, fields: { questions: { responses: [:keyword, :answer] } }
+    render json: @survey, include: 'questions.responses', fields: { questions: { responses: [:keyword, :answer] } }
   end
 
   # POST /surveys
